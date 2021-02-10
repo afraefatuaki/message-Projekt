2c603e4044f39fbe13e8fd3a72e80e12fa8a3bd5
afraefatuaki <afrae@elfatuaki.com>
2021-02-10 21:28:05 +0100

    style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
 let content = document.getElementById('content')
 let empty = document.getElementById('noComment')
 let full = document.getElementById('yourComment')
-function writeHere() {
-    if (content.value == "") {
-        empty.innerHTML = 'Gib Bitte eine Nachricht an'
+// function writeHere() {
+//     if (content.value == "") {
+//         empty.innerHTML = 'Gib Bitte eine Nachricht an'
+//     } else {
+//         full.innerHTML = content.value
+//     }
+// }
 
-    } else {
+function writeHere() {
+    if ((content.value).length > 0) {
         full.innerHTML = content.value
+    } else {
+        empty.innerHTML = 'Gib Bitte eine Nachricht an'
     }
 }
+
+// Both options work, Just wanted to try :)  
+
